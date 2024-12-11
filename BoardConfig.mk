2a250fcc8467b828a114eda0ca6a116ac80474f9
Kai Jones <kvjones0243@gmail.com>
2024-12-11 13:48:15 -0800

    gta8wifi: Initial skeleton tree
    Change-Id: Ifcb669f3c41e530cc370bf126fb9695ee7c27643

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/samsung/gta8wifi
+
+# Inherit the proprietary files
+include vendor/samsung/gta8wifi/BoardConfigVendor.mk
